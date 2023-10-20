b17a6d55b8306f209ea194e958c7c8cb36fb1732
mccrusader <50123715+mccrusader@users.noreply.github.com>
2023-10-20 13:55:01 -0400

    feat: Added a homescreen, options, and beter data.

--- monitor_guac.py
+++ monitor_guac.py
@@ -1,35 +1,111 @@
-import guacamole
+"""
+Guacamole Monitor
+"""
+
 import json
 import yaml
 from flask import Flask, render_template
+from guacamole import session as guac_connection
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
-with open('config.yaml', 'r') as config_file:
+with open('config.yaml', 'r', encoding='utf-8') as config_file:
     config = yaml.safe_load(config_file)
 
-GUAC_URL = config['guacamole_url']
-USERNAME = config['username']
-PASSWORD = config['password']
-API_TOKEN = config['api_token']
 
-session = guacamole.session(GUAC_URL, "mysql", USERNAME, PASSWORD)
+gconn = guac_connection(config['host'],
+                        config['data_source'],
+                        config['username'],
+                        config['password'])
+
+
+@app.route('/')
+def index():
+    """
+    A function that serves as the handler for the root URL ("/"). 
+    It returns the rendered template named "index.html".
+    """
+    return render_template('index.html')
+
 
 @app.route('/active_connections', methods=['GET'], defaults={'timeout': None})
 @app.route('/active_connections/<timeout>', methods=['GET'])
-def active_connections_route(timeout):
-    sessions = json.loads(session.list_connections())
-    active_connections = {}
+def active_connections_route(timeout=None):
+    """
+    Renders the active connections from the server.
 
-    for identifier, connection in sessions.items():
-        if connection.get("activeConnections", 0) == 1:
-            name = connection.get('name')
-            column_name = name.split('.')[0]  # Get the part before the first "."
-            active_connections.setdefault(column_name, []).append(name)
+    Returns:
+        str: The rendered HTML template for displaying the active connections.
+    """
 
-    total_active_connections = sum(len(names) for names in active_connections.values())
-    
-    return render_template('active_connections.html', active_connections=active_connections, total_active_connections=total_active_connections)
+    active_instances = json.loads(
+        gconn.list_connections(active=True)
+    )
+    active_data = [
+        {
+            'data': json.loads(
+                gconn.detail_connection(
+                    instance['connectionIdentifier']
+                )
+            ),
+            'username': instance['username'],
+        }
+        for instance in active_instances.values()
+    ]
+
+    active_conns = {}
+
+    for conn in active_data:
+        conn_name = conn['data'].get('name')
+        column_name = conn_name.split('.')[0]
+        active_conns.setdefault(
+            column_name, []
+        ).append({
+            'connection': conn_name,
+            'username': conn['username']
+        })
+
+    return render_template('active_connections.html',
+                           active_conns=active_conns,
+                           active_conns_length=len(active_conns))
+
+
+@app.route('/active_users', methods=['GET'], defaults={'timeout': None})
+@app.route('/active_users/<timeout>', methods=['GET'])
+def active_users_route(timeout=None):
+    """
+    Renders the active connections from the server.
+
+    Returns:
+        str: The rendered HTML template for displaying the active connections.
+    """
+
+    active_instances = json.loads(gconn.list_connections(active=True))
+    active_usernames = set(
+        instance['username']
+        for instance in active_instances.values()
+    )
+    active_user_data = [
+        json.loads(gconn.detail_user(user))
+        for user in active_usernames
+    ]
+    active_users = {}
+
+    for user in active_user_data:
+        user_name = user.get('username')
+        column_name = user['attributes'].get(
+            'guac-organization', 'No Organization'
+        )
+        active_users.setdefault(
+            column_name, []
+        ).append(user_name)
+
+    total_active_users = len(active_users)
+
+    return render_template('active_users.html',
+                           active_users=active_users,
+                           total_active_users=total_active_users)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
