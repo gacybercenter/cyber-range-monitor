c07e64e2913a34a4e4d3c176400e10c122c95edc
mccrusader <50123715+mccrusader@users.noreply.github.com>
2023-11-08 17:19:34 -0500

    feat: Added a users tab.

--- range_monitor/main.py
+++ range_monitor/main.py
@@ -6,10 +6,11 @@ import os
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for, current_app
 )
-# from werkzeug.exceptions import abort
+from werkzeug.exceptions import abort
+from werkzeug.security import check_password_hash, generate_password_hash
 
-# from range_monitor.db import get_db
-from range_monitor.auth import login_required
+from range_monitor.db import get_db
+from range_monitor.auth import login_required, admin_required
 
 bp = Blueprint('main', __name__)
 
@@ -30,6 +31,115 @@ def index():
     plugins = os.listdir(plugins_dir)
 
     return render_template('main/index.html', plugins=plugins)
+
+
+@bp.route('/users')
+@login_required
+def users():
+    """
+    Route decorator for the index page.
+
+    Returns:
+        str: The rendered HTML template for the index page.
+
+    Raises:
+        None
+    """
+
+    db = get_db()
+    users = db.execute(
+        'SELECT p.id, username, password, permission'
+        ' FROM user p'
+        ' ORDER BY p.id DESC'
+    ).fetchall()
+
+    return render_template('main/users.html', users=users)
+
+
+@bp.route('/create_user', methods=('GET', 'POST'))
+@admin_required
+def create_user():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        permission = request.form['permission']
+        error = None
+
+        if not username or not password or not permission:
+            error = 'Fill in all required fields.'
+
+        if error is None:
+            try:
+                db = get_db()
+                db.execute(
+                    "INSERT INTO user (username, password, permission) VALUES (?, ?, ?)",
+                    (username, generate_password_hash(password), 'user'),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("main.users"))
+
+        flash(error)
+
+    return render_template('main/create_user.html')
+
+
+def get_user(id):
+    user = get_db().execute(
+        'SELECT p.id, username, password, permission'
+        ' FROM user p'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if user is None:
+        abort(404, f"User id {id} doesn't exist.")
+
+    if user['permission'] not in ['admin', 'user']:
+        abort(403)
+
+    return user
+
+
+@bp.route('/<int:id>/edit_user', methods=('GET', 'POST'))
+@admin_required
+def edit_user(id):
+    user = get_user(id)
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        permission = request.form['permission']
+        error = None
+
+        if not username or not password or not permission:
+            error = 'Fill in all required fields.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE user SET username = ?, password = ?, permission = ?'
+                ' WHERE id = ?',
+                (username, generate_password_hash(password), permission, id)
+            )
+            db.commit()
+            return redirect(url_for('main.users'))
+
+    return render_template('main/edit_user.html', user=user)
+
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@admin_required
+def delete(id):
+    get_user(id)
+    db = get_db()
+    db.execute('DELETE FROM user WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('main.index'))
 
 
 
--- range_monitor/schema.sql
+++ range_monitor/schema.sql
@@ -1,18 +1,19 @@
 DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS config;
 DROP TABLE IF EXISTS user_permissions;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   username TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL,
   permission TEXT NOT NULL,
   FOREIGN KEY (permission) REFERENCES user_permissions (permission)
 );
 
-INSERT INTO user (username, password, permission)
+INSERT INTO user (created, username, password, permission)
 VALUES
-  ('Administrator',
+  (CURRENT_TIMESTAMP,
+  'Administrator',
   'scrypt:32768:8:1$OZrgrecSOwvEYxBR$b7b5fc887dc6a227c8eb35e22c602e5a62f9305d8458a898bf42a920b84e03b282d8187410d5ad989af71d1120c7b5213466afb42d1a133100101ea06a02da1e',
   'admin');
 
