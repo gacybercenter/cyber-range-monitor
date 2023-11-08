f226c91b8e91241f886ad6f82a7fca822d376d6e
mccrusader <50123715+mccrusader@users.noreply.github.com>
2023-11-08 13:23:39 -0500

    fix: Unit test refactored.

--- tests/data.sql
+++ tests/data.sql
@@ -5,7 +5,3 @@ VALUES
 INSERT INTO user_permissions (user_id, permission)
 VALUES
   (2, 'user');
-
-INSERT INTO config (title, body, author_id, created)
-VALUES
-  ('test title', 'test' || x'0a' || 'body', 1, '2018-01-01 00:00:00');
