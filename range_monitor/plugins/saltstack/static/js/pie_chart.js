452e0efe499d302260f62e5c157dc8cafb190384
snootfracks <allykat653@gmail.com>
2024-06-10 12:26:46 -0400

    [fix]: updated chart to cycle through list of colors regarless of data length

--- range_monitor/plugins/saltstack/static/js/pie_chart.js
+++ range_monitor/plugins/saltstack/static/js/pie_chart.js
@@ -6,31 +6,6 @@ var barColors = [
   "#e0bc74",
   "#e08f74",
   "#e07486",
-  "#7de074",
-  "#d7e074",
-  "#e0bc74",
-  "#e08f74",
-  "#e07486",
-  "#7de074",
-  "#d7e074",
-  "#e0bc74",
-  "#e08f74",
-  "#e07486",
-  "#7de074",
-  "#d7e074",
-  "#e0bc74",
-  "#e08f74",
-  "#e07486",
-  "#7de074",
-  "#d7e074",
-  "#e0bc74",
-  "#e08f74",
-  "#e07486",
-  "#7de074",
-  "#d7e074",
-  "#e0bc74",
-  "#e08f74",
-  "#e07486",
 ];
 const piechart = new Chart("piechart", {
   type: "doughnut",
@@ -56,8 +31,13 @@ function updateGraph() {
     .then(data => {
       const xvalue = data.x;
       const yvalue = data.y;
+      const bgColors = [];
+      for (let i = 0; i < xvalue.length; i++) {
+        bgColors.push(barColors[i % barColors.length]);
+      }
       piechart.data.labels = xvalue;
       piechart.data.datasets[0].data = yvalue;
+      piechart.data.datasets[0].backgroundColor = bgColors;
       piechart.update();
     })
     .catch(error => {
