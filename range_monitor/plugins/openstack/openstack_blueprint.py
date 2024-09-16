dc14bde9d30d422285e3af839a646fa8414ca325
will <wburnley.professional@gmail.com>
2024-09-16 08:57:16 -0400

    feat: prettier openstack plugin page

--- range_monitor/plugins/openstack/openstack_blueprint.py
+++ range_monitor/plugins/openstack/openstack_blueprint.py
@@ -1,4 +1,5 @@
 import flask
+import openstack
 import json
 from range_monitor.auth import login_required, admin_required, user_required
 from .stack_class import StackConnection
@@ -30,12 +31,12 @@ class OpenStackBlueprint:
     def register_routes(self):
         @self.blueprint.route("/")
         @login_required
-        def overview() -> str:
+        def dashboard() -> str:
             """
-            Renders a general dashboard overview.
+            Renders a general dashboard.
 
             Returns:
-                str: The rendered HTML template for the dashboard overview.
+                str: The rendered HTML template for the dashboard.
             """
             
             instances_summary = {
@@ -56,10 +57,79 @@ class OpenStackBlueprint:
                 "instances_summary": instances_summary,
                 "networks_summary": networks_summary
             }
-
-            return flask.render_template("openstack/overview.html", data=data)
+            
+            return flask.render_template("openstack_base.html")
         
-        @self.blueprint.route("/api/overview_data", methods=["GET"])
+        @self.blueprint.route("/systems_health/")
+        @login_required
+        def systems_health():
+            servers_summary = {
+                "active_servers": sum(
+                    1 for server in self.connection.servers if server.status == "ACTIVE"
+                ),
+                "total_servers": len(self.connection.servers)
+            }
+            
+            networks_summary = {
+                "active_networks": sum(
+                    1 for network in self.connection.networks if network.status == "ACTIVE"
+                ),
+                "total_networks": len(self.connection.networks)
+            }
+            
+            data = {
+                "servers_summary": servers_summary,
+                "networks_summary": networks_summary
+            }
+            
+            return flask.render_template("openstack/systems_health.html", data=data)
+        
+        @self.blueprint.route("/topology/", methods=["GET"])
+        @login_required
+        def topology() -> str:
+            """
+            Renders the OpenStack topology.
+
+            Returns:
+                str: The rendered HTML template for the topology page.
+            """
+            return flask.render_template("openstack/topology.html")
+        
+        @self.blueprint.route("/performance/")
+        @login_required
+        def performance():
+            cpu_usage_data = []
+            memory_usage_data = []
+            for server in self.connection.servers:
+                if server.status != "ACTIVE":
+                    continue
+                
+                diagnostics = self.connection.connection.compute.get_server_diagnostics(server.id)
+                cpu_stats = diagnostics.get("cpu_details", [])
+                memory_stats = diagnostics.get("memory_details", [])
+                
+                total_cpu = sum([cpu["time"] for cpu in cpu_stats if "time" in cpu])
+                total_memory = sum([memory["usage"] for memory in memory_stats if "usage" in memory])
+                
+                cpu_usage_data.append({
+                    "server_id": server.id,
+                    "server_name": server.name,
+                    "cpu_usage": total_cpu
+                })
+                memory_usage_data.append({
+                    "server_id": server.id,
+                    "server_name": server.name,
+                    "memory_usage": total_memory
+                })
+            
+            performance_data = {
+                "cpu_usage": cpu_usage_data,
+                "memory_usage": memory_usage_data
+            }
+            print(f"\n\n\n\n{performance_data["cpu_usage"][0]["server_id"]}n\n\n\n")
+            return flask.render_template("openstack/performance.html", data=performance_data)
+        
+        @self.blueprint.route("/api/overview_data/", methods=["GET"])
         @login_required
         def api_overview_data() -> flask.jsonify:
             """
@@ -88,3 +158,6 @@ class OpenStackBlueprint:
                 "networks_summary": networks_summary
             }
             return flask.jsonify(data)
+        
+        
+            
