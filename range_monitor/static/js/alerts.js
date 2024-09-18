50411f674de6227ec1df2d106f0f074643eeaace
rhawk117 <hawkinsry19@gmail.com>
2024-09-18 13:04:36 -0400

    feat: finished plugin cards that now have cute icons!

---
+++ range_monitor/static/js/alerts.js
@@ -0,0 +1,13 @@
+const items = document.querySelectorAll(".alert-item");
+const alertContent = document.getElementById("alertContent");
+
+function toggleAlerts(btn) {
+  btn.classList.toggle("rotate");
+  alertContent.classList.toggle("show");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  items.forEach((item) => {
+    item.classList.add("show");
+  });
+});
---
+++ range_monitor/static/js/plugins.js
@@ -0,0 +1,4 @@
+const main = document.querySelector("main");
+document.addEventListener("DOMContentLoaded", () => {
+    main.classList.add("star-background");
+});
