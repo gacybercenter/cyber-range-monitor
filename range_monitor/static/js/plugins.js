bf85e26b89970b69e5970a3e1a8b50bca5862805
rhawk117 <hawkinsry19@gmail.com>
2024-09-19 11:49:32 -0400

    refacotr: refactored

--- range_monitor/static/js/plugins.js
+++ range_monitor/static/js/plugins.js
@@ -1,4 +1,6 @@
 const main = document.querySelector("main");
 document.addEventListener("DOMContentLoaded", () => {
-    main.classList.add("star-background");
+    main.classList.add("star-background");  
+    // only have 2 do this bc I didn't want to set in base template the background
+    // for all 2 be stars
 });
