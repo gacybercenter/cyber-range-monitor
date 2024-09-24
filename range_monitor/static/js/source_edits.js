0d874adc937280ad6f5ca5d8bb9fec9930ca25a4
rhawk117 <hawkinsry19@gmail.com>
2024-09-24 12:54:29 -0400

    feat: javascript for Create User UI and icons macro for easily assigning icons to form labels

---
+++ range_monitor/static/js/source_edits.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const togglePassword = document.querySelector(".toggle-password");
+  const passwordInput = document.getElementById("password");
+  // password visibility toggler
+  togglePassword.addEventListener("click", function () {
+    const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
+    passwordInput.setAttribute("type", type);
+    this.querySelector("i").classList.toggle("fa-eye");
+    this.querySelector("i").classList.toggle("fa-eye-slash");
+  });
+
+  const inputs = document.querySelectorAll(".data-field");
+  inputs.forEach( (input) => {
+    input.addEventListener("mouseover", function () {
+      this.style.boxShadow = "0 0 5px rgba(80, 120, 127, 0.5)";
+    });
+    input.addEventListener("mouseout", function () {
+      this.style.boxShadow = "none";
+    });
+  });
+});
