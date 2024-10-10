34ee4cb389a7245bb4ddf089c44855bab80498f8
rhawk117 <hawkinsry19@gmail.com>
2024-10-10 11:29:45 -0400

    feat: expanding the functionality of alert bar for more flexibility with javascript

--- range_monitor/static/js/components/alerts.js
+++ range_monitor/static/js/components/alerts.js
@@ -3,21 +3,70 @@
 /**
  * adds the UI events and effects
  * for the alert bar if it's found
- * (i.e an element has id of alertContent) 
-*/
-export const initAlerts = () => {
-  const $alertContent = $("#alertContent");
-	if ($alertContent.length === 0) {
-		console.log("No alert content found");
-		return;
-	}
-  
-	$("#alertToggler").on("click", function () {
-		$(this).toggleClass("rotated");
-		$alertContent.toggleClass("show");
-	});
+ * (i.e an element has id of alertContent)
+ */
 
-	$(".alert-item").each(function () {
-		$(this).addClass("show");
-	});
+// WIP: modularizing alert bar
+function initAlertBar() {
+  const $alertContent = $("#alertContent");
+  if ($alertContent.length === 0) {
+    console.log("No alert content or alert bar was found");
+    return;
+  }
+
+  $("#alertToggler").on("click", function () {
+    $(this).toggleClass("rotated");
+    $alertContent.toggleClass("show");
+  });
+
+  $(".alert-item").each(function () {
+    $(this).addClass("show");
+  });
 }
+function alertBarExists() {
+	return $("#alertContent").length > 0;
+}
+function deleteAlertBar() {
+  $(".alert-container").remove();
+}
+function getAlertCount() {
+  return $(".alert-item").length;
+}
+function getAlerts() {
+  return $(".alert-item")
+    .map(function () {
+      return $(this).text();
+    })
+    .get();
+}
+function replayBellShake(element, className) {
+  const bell = $("#alertBell");
+	bell.removeClass("shake");
+  void bell.get(0).offsetWidth;
+  /* chat-gpt says doing line of code above -^
+   * "forces reflow by accessing the offsetWidth
+   * (this makes the browser "forget" the removed class)" 
+  */
+  bell.addClass("shake");
+}
+// function addAlert(message) {
+//   const $alertContent = $("#alertContent");
+//   if ($alertContent.length === 0) {
+//     console.log("No alert content or alert bar was found");
+//     return;
+//   }
+//   const $alerts = $(".alert-list li");
+// 	/* 1 alert means their were no alerts and the 
+// 	 * "No alerts to display message" is the only ".alert-item"
+// 	 * or <li>
+// 	*/
+// 	const alertNum = 
+// 	if($alerts.length === 1) {
+// 		$alerts.first().remove(); 
+//   }
+	
+
+
+
+
+// }
