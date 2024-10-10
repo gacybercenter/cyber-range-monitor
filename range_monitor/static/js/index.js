016c0147f5c6f3ba26d91531c60fac03793397bf
rhawk117 <hawkinsry19@gmail.com>
2024-10-10 13:09:59 -0400

    feat: eye fades on index and working on making plugin fade in

--- range_monitor/static/js/index.js
+++ range_monitor/static/js/index.js
@@ -1,5 +1,26 @@
 import { Typer } from "./effects/type_effect.js";
 
 $(document).ready(function () {
-	
+  typePluginHeader();
+  typeHeroText();
 });
+
+function typeHeroText() {
+  const heroTyper = new Typer("#heroText", 50, 1500);
+  const userName = heroTyper.$element.data("username");
+  heroTyper.start(
+    [
+      `Welcome ${userName}, enjoy your stay.`,
+      "Big Brother is always watching ",
+    ],
+    () => {
+      $("#heroText").append($("<i></i>").addClass("fas fa-eye iconFade"));
+    }
+  );
+}
+function typePluginHeader() {
+  const pluginTyper = new Typer("#pluginHeader", 50, 1500);
+  pluginTyper.start(["Plugins"], () => {
+    pluginTyper.$element.append($("<i></i>").addClass("fas fa-plug iconFade"));
+  });
+}
