9717033534d39b2c070141f2bbbff3379b4c9c11
rhawk117 <hawkinsry19@gmail.com>
2024-11-18 12:17:56 -0500

    feat: implemented better request handling.

--- range_monitor/plugins/guacamole/static/js/topology.js
+++ range_monitor/plugins/guacamole/static/js/topology.js
@@ -1,13 +1,12 @@
 // static/js/topology.js
-import { Topology } from "./topology/main_ui.js";
+import { topology } from "./topology/main_ui.js";
 
 import {
 	NavigationHints 
 } from "./topology/user-interface/ui_hints.js";
 
 $(function () {
-	const topology = new Topology();
 	NavigationHints.init();
-	topology.statusUI.loading();
+	topology.loadScreen.loading();
 	topology.render(true);
 });
--- range_monitor/plugins/guacamole/static/js/topology/main_ui.js
+++ range_monitor/plugins/guacamole/static/js/topology/main_ui.js
@@ -1,159 +1,60 @@
 // topology/display.js
 import { ConnectionData } from "./data/context.js";
-import { setupD3, GraphAssets } from "./user-interface/assets.js";
+import { GraphUI } from "./user-interface/assets.js";
 import { RequestHandler } from "./data/request_handler.js";
 import { LoadScreen } from "./user-interface/ui_hints.js";
 import { updateScheduler } from "./refresh.js";
 import { initSettingsModal, UserSettings } from "./user-settings.js";
 
-
-
-/* 
-	TODO
-
-	-Move assets into seperate class that 
-	handles rendering the nodes & managing 
-	simulation passed data by the Context
-
-	-Implement UserSettings 
-
-	-Reimplement fetch routine 
-*/
-
-
-
-class GraphUI {
-	constructor() {
-		const { svg, container } = setupD3.initSVG();
-		this.svg = svg;
-		this.container = container;
-		setupD3.setupFilters(container);
-		this.simulation = setupD3.setupSimulation(svg);
-		this.drag = this.setupDrag();
-		this.assets = new GraphAssets(this.container);
-		this.tickAdded = false;
-	}
-	/**
-	 * @returns {function} - a drag event handler
-	 */
-	setupDrag() {
-		const dragStarted = (event, d) => {
-			if (!event.active) {
-				this.simulation.alphaTarget(0.1).restart();
-			} 
-			d.fx = d.x;
-			d.fy = d.y;
-		};
-
-		const dragged = (event, d) => {
-			d.fx = event.x;
-			d.fy = event.y;
-		};
-
-		const dragEnded = (event, d) => {
-			if (!event.active) {
-				this.simulation.alphaTarget(0.1).restart();
-			}
-			d.fx = null;
-			d.fy = null;
-		};
-
-		return d3
-			.drag()
-			.on("start", dragStarted)
-			.on("drag", dragged)
-			.on("end", dragEnded);
-	}
-	/**
-	 * @param {ConnectionData} context 
-	 * @param {String[]} userSelection 
-	 */
-	setAssetData(context, clonedEdges, userSelection) {
-		this.assets.setEdges(clonedEdges, context.nodeMap);
-		this.assets.setNodes(this.drag, userSelection, context);
-		this.assets.setLabels(context.nodes);
-		this.assets.setIcons(context.nodes);
-		this.simulation.nodes(context.nodes);
-		this.simulation.force("link").links(clonedEdges);
-	}
-	restartSimulation(alphaValue) {
-		this.simulation
-			.alpha(alphaValue)
-			.alphaTarget(0.1)
-			.restart();
-
-		if(this.tickAdded) {
-			return;
-		}
-		console.log("Adding Tick Event Listener");
-		this.simulation.on("tick", () => {
-			this.assets.onTick();
-		});
-		this.tickAdded = true;
-	}
-}
-
-export class Topology {
-	constructor() {
-		this.display = new GraphUI();
-		this.userSettings = new UserSettings();
-		this.statusUI = new LoadScreen();
-		this.updateScheduler = updateScheduler; 
-		this.context = null;
-		this.userSelection = [];
-	}
-	handleRenderError(error, shouldRecreate) {
-		if (!shouldRecreate) {
+export const topology = {
+	display: new GraphUI(),
+	userSettings: new UserSettings(),
+	loadScreen: new LoadScreen(),
+	updateScheduler: updateScheduler,
+	context: null,
+	userSelection: [],
+	handleRenderError(error, isFirstRender) {
+		if (!isFirstRender) {
 			this.updateScheduler.pause();
 			alert(`The topology failed to refresh and will not be updated: ${error.message}`);
 			return;
 		}
-		const $retryBtn = this.statusUI.toErrorMessage(error.message);
+		const $retryBtn = this.loadScreen.toErrorMessage(error.message);
 		$retryBtn.on("click", async () => {
-			this.statusUI.toLoading();
+			this.loadScreen.toLoading();
 			await this.render(true);
 		});
-	}
-	/**
-	 * @param {bool} shouldRecreate
-	 * @returns {Promise<void>}
-	 */
-	async render(shouldRecreate = false) {
+	},
+	async render(isFirstRender = false) {
 		try {
-			await this.buildTopology(shouldRecreate);
+			await this.buildTopology(isFirstRender);
 		} catch (error) {
 			console.log(`[RENDER_ERROR] -> ${error.message}`);
-			this.handleRenderError(error, shouldRecreate);
+			this.handleRenderError(error, isFirstRender);
 		}
-	}
-
-	async buildTopology(shouldRecreate) {
-		// ignore IDE warning, you need to await this 
-		const [apiData, error] = await RequestHandler.fetchGuacAPI();
-		if(error) {
-			throw new Error(error);
-		}
-		
+	},
+	async buildTopology(isFirstRender) {
+		const apiData = await getTopologyData(15000, 3);
 		if(this.context) {
-			this.updateTopology(apiData, shouldRecreate);
+			this.updateTopology(apiData, isFirstRender);
 		} else {
-			this.createTopology(apiData, shouldRecreate);
+			this.createTopology(apiData, isFirstRender);
 		}
-		
+		this.afterRender();		
+	},
+	afterRender() {
 		const { refreshEnabled } = this.userSettings;
 		const { isRunning } = this.updateScheduler;
-
 		if(refreshEnabled && !isRunning) {
 			this.updateScheduler.start();
 		} else if(!refreshEnabled && updateScheduler.isRunning) {
 			this.updateScheduler.pause();
 		}
-	}
-
-	createTopology(apiData, shouldRecreate) {
+	},
+	createTopology(apiData, isFirstRender) {
 		this.context = new ConnectionData();
 		this.context.build(apiData);
-		this.renderTopology(shouldRecreate, false);
+		this.renderTopology(isFirstRender, false);
 		
 		if(this.updateScheduler.isRunning) {
 			return;
@@ -162,31 +63,27 @@ export class Topology {
 			await this.render();
 		});
 		this.updateScheduler.start();
-	}
+	},
 
-	updateTopology(data, shouldRecreate) {
+	updateTopology(data, isFirstRender) {
 		const filteredData = ConnectionData.filterByStatus(
 			data, this.userSettings.showInactive
 		);
 
 		const hasChanged = this.context.refreshContext(filteredData);
 		if(hasChanged) {
-			this.renderTopology(shouldRecreate, hasChanged);
+			this.renderTopology(isFirstRender, hasChanged);
 		}
-	}
+	},
 	
-	renderTopology(shouldRecreate, hasChanged) {
-		/* NOTE 
-			for some reason d3 mutates the edges array and 
-			wasted 2 hours of my life try it yourself and 
-			watch as time evaporates 
-		*/
+	renderTopology(isFirstRender, hasChanged) {
+		// d3 mutates edges for some reason, so clone it (trust me)
 		const clonedEdges = this.context.edges.map((edge) => ({ ...edge }));	
 		this.display.setAssetData(this.context, clonedEdges, this.userSelection)
 		
-		console.log(`Should Recreate: ${shouldRecreate}, Has Changed: ${hasChanged}`);	
+		console.log(`Should Recreate: ${isFirstRender}, Has Changed: ${hasChanged}`);	
 		let alphaValue;
-		if(shouldRecreate) {
+		if(isFirstRender) {
 			alphaValue = 1;
 		} else if(hasChanged) {
 			alphaValue = 0.1;
@@ -196,11 +93,11 @@ export class Topology {
 		console.log(`Resulting Alpha Value: ${alphaValue}`);
 		this.display.restartSimulation(alphaValue);
 		
-		if(shouldRecreate) {
-			this.statusUI.hide();
+		if(isFirstRender) {
+			this.loadScreen.hide();
 			initSettingsModal(this);
 		}
-	}
+	},
 	async toggleRefresh() {
 		this.userSettings.refreshEnabled = !this.userSettings.refreshEnabled;
 		if (this.userSettings.refreshEnabled) {
@@ -208,10 +105,61 @@ export class Topology {
 		} else {
 			this.updateScheduler.pause();
 		}
-	}
+	},
 	async toggleInactive() {
 		this.userSettings.showInactive = !this.userSettings.showInactive;
 		this.updateScheduler.pause();
 		await this.render();
 	}
-}
+};
+
+/**
+ * 
+ * @param {Number} timeout - refresh speed - 2,500 
+ * @param {Number} retries 
+ * @returns 
+ */
+async function getTopologyData(timeout = 15000, retries = 1) {
+	if(retries > 5) {
+		throw new Error("Use a lower value for retries allowed to avoid overloading the server");
+	}
+	const controller = new AbortController();
+	const { signal } = controller;
+	const timeoutId = setTimeout(() => controller.abort(), timeout);
+	console.log(`[FETCH] -> Attempting to fetch topology data, timeout: ${timeout}ms with ${retries} retries`);
+	try {
+		const response = await fetch("api/topology_data", { signal });
+		if(!response.ok) {
+			throw new Error(`Failed to fetch topology data: ${response.statusText}`);
+		}
+		const data = await response.json();
+		if(!data || !data.nodes) {
+			throw new Error("Invalid topology data received likely due to missing data");
+		}
+		clearTimeout(timeoutId);
+		return data.nodes;
+	} catch(err) {
+		return onFetchError(err, timeout, retries, timeoutId);
+	}
+};
+
+const onFetchError = async (err, timeout, retries, timeoutId) => {
+	clearTimeout(timeoutId);
+	let errorMsg = err.message;
+	if(err.name === "AbortError") {
+		errorMsg = "Timeout Error: the request to the API timed out after multiple attempts.";
+		timeout += 5000; // timed out due to network issues -> increase timeout
+	}
+	if(retries <= 1) {
+		throw new Error(errorMsg);
+	}
+	console.log(`[FETCH_ERROR] -> Request failed, ${retries - 1} attempts remaining`);
+	// to avoid overloading server w/ reqs -v
+	await new Promise((resolve) => setTimeout(resolve, 3000));
+	return await getTopologyData(timeout, retries - 1);
+};
+
+
+
+
+
