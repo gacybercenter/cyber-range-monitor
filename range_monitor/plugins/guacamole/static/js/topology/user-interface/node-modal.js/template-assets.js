89e8fd674a03c62ad9064f9a7913acac0b6ece18
rhawk117 <hawkinsry19@gmail.com>
2024-12-02 13:18:44 -0500

    chore: updated template assets support new templates.

--- range_monitor/plugins/guacamole/static/js/topology/user-interface/node-modal.js/template-assets.js
+++ range_monitor/plugins/guacamole/static/js/topology/user-interface/node-modal.js/template-assets.js
@@ -1,5 +1,11 @@
 
-const assets = {
+
+/**
+ * @enum {string}
+ * contains the asset IDs to reduce
+ * errors and improve consitency 
+ */
+const assetIds = {
 	checkbox: "modalCheckbox",
 	filter: "filterTab",
 	pager: "checkboxPager",
@@ -7,23 +13,140 @@ const assets = {
   tab: "modalTab",
   field: "modalField",
   collapsible: "modalCollapse",
+  nodeBtn: "nodeBtn",
 };
 
+export const assetFactory = {
+  /**
+   * @summary
+   * creates a field with a title and value
+   * @param {string} title 
+   * @param {string} value 
+   * @returns {JQuery<HTMLElement>} 
+   */
+  createField(title, value) {
+    const $field = components.cloneAsset(assetIds.field);
+    $field.find(".field-title").text(title);
+    $field.find(".field-value").text(value);
+    return $field;
+  },
+  createNodeBtn(btnText, btnClass, btnIcons) {
+    const $btn = components.cloneAsset(assetIds.nodeBtn);
+    $btn.addClass(btnClass);
+    $btn.find(".node-btn-text").text(btnText);
+    const $btnIcon = $btn.find(".node-btn-icon");
+    const { staticIcon, hoverIcon } = btnIcons;
+    $btnIcon
+      .addClass(staticIcon)
+      .hover(
+        function() {
+          $(this).removeClass(staticIcon).addClass(hoverIcon);
+        }, 
+        function() {
+          $(this).removeClass(hoverIcon).addClass(staticIcon);
+        }
+      );
+    return $btn;
+  },
+  /**
+   * @summary
+   * creates a collapsible section of content,
+   * events are added by Modal class 
+   * @param {string} title 
+   * @returns {Object{ $container: JQuery<HTMLElement>, $content: JQuery<HTMLElement> }}
+   */
+  createCollapse(title) {
+    const $collapse = components.cloneAsset(assetIds.collapsible);
+    const $content = $collapse.find(".collapsible-content");
+    $collapse.find(".collapse-title").text(title);
+    return { 
+      $container: $collapse, 
+      $content: $content 
+    };
+  },
+  /**
+   * creates the tab window in the modal
+   * @param {string} title - title of the tab
+   * @param {string} fasIcon - fas icon class 
+   * @returns {JQuery<HTMLElement>}
+   */
+  createTab(title, fasIcon) {
+    const $tab = components.cloneAsset(assetIds.tab);
+    $tab.find(".tab-text").text(title);
+    $tab.find(".tab-icon").addClass(fasIcon);
+    return $tab;
+  },
+  /**
+   * creates the container for tab content
+   * @param {string} tabId 
+   * @param {JQuery<HTMLElement>} $contents 
+   * @returns {JQuery<HTMLElement>}
+   */
+  createTabContent($contents) {
+    return $("<div>")
+      .addClass("tab-content")
+      .attr("role", "tabpanel")
+      .append($contents);
+  },
+  /**
+   * @summary
+   * creates a checkbox in the group selector
+   * for connection groups
+	 * @param {ConnectionNode} connection
+	 * @returns {JQuery<HTMLElement>}
+	 */
+	createCheckbox(connection) {
+		const $checkbox = components.cloneAsset(assetIds.checkbox);
+		$checkbox
+			.attr("data-node-id", connection.identifier)
+			.attr("data-active", connection.isActive())
+			.find(".checkbox-label")
+			.text(connection.name)
+			.append(connection.getOsIcon());
+		return $checkbox;
+	},
+	/**
+   * @summary
+   * creates a "filter" tab / button in the
+   * group selector 
+	 * @param {Object} filterConfig - { text, count, icon, dataFilter }
+	 * @returns {JQuery<HTMLElement>}
+	 */
+	createFilter(filterConfig) {
+		const { text, count, icon, dataFilter } = filterConfig;
+		const $filter = components.cloneAsset(assetIds.filter);
+		$filter
+			.attr("data-filter", dataFilter)
+			.find(".filter-label")
+			.prepend(`${text} (${count}) `)
+			.find(".filter-icon")
+			.addClass(icon);
+		return $filter;
+	},
+};
+
+
+
+
 /**
+ * @summary
  * manages & caches the template tags / document fragments
- * for commonly rendered assets, so they can be cloned
+ * for commonly rendered assets, so they can be cloned. 
+ * this approach reduces the number of times the DOM is
+ * queried for the same elements and makes rendering the same HTML
+ * over and over again less performance intensive.
  */
 const components = {
-	assets: {},
+	assetIds: {},
 	/**
 	 * @param {string} templateId
 	 * @returns {JQuery<HTMLElement>}
 	 */
 	cloneAsset(templateId) {
-		if (!this.assets[templateId]) {
+		if (!this.assetIds[templateId]) {
 			this.registerAsset(templateId);
 		}
-		return this.assets[templateId].clone();
+		return this.assetIds[templateId].clone();
 	},
 	/**
 	 * @param {string[]} templateId
@@ -34,59 +157,10 @@ const components = {
 			throw new Error(`[COMPONENT_ERROR] Template with id ${templateId} not found`);
 		}
 		const cloned = template.content.cloneNode(true).children[0];
-		this.assets[templateId] = $(cloned);
+		this.assetIds[templateId] = $(cloned);
 	}
 };
 
-export const assetFactory = {
-  /**
-	 * @param {ConnectionNode} connection
-	 * @returns {JQuery<HTMLElement>}
-	 */
-	createCheckbox(connection) {
-		const $checkbox = components.cloneAsset(assets.checkbox);
-		$checkbox
-			.attr("data-node-id", connection.identifier)
-			.attr("data-active", connection.isActive())
-			.find(".checkbox-label")
-			.text(connection.name)
-			.append(connection.getOsIcon());
-		return $checkbox;
-	},
-	/**
-	 * @param {Object} filterConfig - { text, count, icon, dataFilter }
-	 * @returns {JQuery<HTMLElement>}
-	 */
-	createFilter(filterConfig) {
-		const { text, count, icon, dataFilter } = filterConfig;
-		const $filter = components.cloneAsset(assets.filter);
-		$filter
-			.attr("data-filter", dataFilter)
-			.find(".filter-label")
-			.prepend(`${text} (${count}) `)
-			.find(".filter-icon")
-			.addClass(icon);
-		return $filter;
-	},
-  createField(title, value) {
-    const $field = components.cloneAsset(assets.field);
-    $field.find(".field-title").text(title);
-    $field.find(".field-value").text(value);
-    return $field;
-  },
-  createCollapse(title) {
-    const $collapse = components.cloneAsset(assets.collapsible);
-    const $content = $collapse.find(".collapsible-content");
-    $collapse.find(".collapse-title").text(title);
-    return { $container: $collapse, $content: $content };
-  },
-  createTab(title, fasIcon) {
-    const $tab = components.cloneAsset(assets.tab);
-    $tab.find(".tab-text").text(title);
-    $tab.find(".tab-icon").addClass(fasIcon);
-    return $tab;
-  }
-};
 
 
 
