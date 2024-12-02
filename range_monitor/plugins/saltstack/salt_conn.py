7102f5dd3a36fb64c1aa7173131ff44a9a55c549
snootfracks <allykat653@gmail.com>
2024-12-02 12:05:30 -0500

    chore: added salt_cache to be used for session vars

--- range_monitor/plugins/saltstack/salt_conn.py
+++ range_monitor/plugins/saltstack/salt_conn.py
@@ -12,7 +12,12 @@ def execute_run_cmd(cmd):
   data_source = salt_call.salt_conn()
   return salt_call.execute_function(data_source['username'], data_source['password'], data_source['endpoint'], "monitor.salt_run_cmd", cmd)
 
-
+"""
+cached information for session
+"""
+salt_cache = {
+  'hostname': None
+}
 
 """
 called in the jobs route to collect all cached jobs
@@ -40,16 +45,18 @@ returns: json returned by salt API cmd without "{'return': [{'salt-dev':" in fro
 args = [cmd, tgt, [args]]
 """
 def get_all_minions():
-  data_source = salt_call.salt_conn()
-  hostname = data_source['hostname']
   cmd = ['grains.items', '*']
   json_data = execute_local_cmd(cmd)
+
+  if salt_cache['hostname'] == None:
+    data_source = salt_call.salt_conn()
+    salt_cache['hostname'] = data_source['hostname']
 
   if 'API ERROR' in json_data:
     print("BAD DATA SOURCE FOUND IN get_all_minions")
     return False
   
-  minion_data = parse.clean_minion_data(json_data, hostname)
+  minion_data = parse.clean_minion_data(json_data, salt_cache['hostname'])
   minion_data = parse.sort_minions_by_role(minion_data)
 
   return minion_data
@@ -65,20 +72,21 @@ def get_specified_minion(minion_id):
   load_cmd = ['status.loadavg', minion_id]
   ipmi_cmd = ['grains.item', minion_id, ['ipmi']]
 
-  data_source = salt_call.salt_conn()
-  hostname = data_source['hostname']
-
   uptime_data = {'uptime_data': execute_local_cmd(uptime_cmd)}
   load_data = {'load_data': execute_local_cmd(load_cmd)}
   ipmi_data = {'ipmi_data': execute_local_cmd(ipmi_cmd)}
 
   data_list = [uptime_data, load_data, ipmi_data]
+  
+  if salt_cache['hostname'] == None:
+    data_source = salt_call.salt_conn()
+    salt_cache['hostname'] = data_source['hostname']
 
   if 'API ERROR' in data_list:
     print("BAD DATA SOURCE FOUND IN get_all_minions")
     return False
   
-  minion_data = parse.individual_minion_data(data_list, hostname)
+  minion_data = parse.individual_minion_data(data_list, salt_cache['hostname'])
   return minion_data
 
 
@@ -87,14 +95,17 @@ called in the /minions/<string:minion_id> route to get advanced minion data
 returns: json returned by salt API cmd without "{'return': [{'salt-dev':" in front
 args = [cmd, tgt, [args]]
 """
-def get_ipmi_data(minion_id):
+def get_cpu_temp(minion_id):
   cmd = ['grains.item', minion_id, ['ipmi']]
   ipmi_data = execute_local_cmd(cmd)
 
   if 'API ERROR' in ipmi_data:
-    print("BAD DATA SOURCE FOUND IN get_ipmi_data")
+    print("BAD DATA SOURCE FOUND IN get_cpu_data")
     return False
   
+  ## parse JSON to only return CPU temp
+
+  print (ipmi_data)
   return ipmi_data
 
 
@@ -122,10 +133,12 @@ args = [cmd, tgt, [args]]
 """
 def get_minion_count():
   cmd = ["manage.up"]
-  data_source = salt_call.salt_conn()
-  hostname = data_source['hostname']
   minions = execute_run_cmd(cmd)
 
-  data = parse.count_roles(minions, hostname)
+  if salt_cache['hostname'] == None:
+    data_source = salt_call.salt_conn()
+    salt_cache['hostname'] = data_source['hostname']
+
+  data = parse.count_roles(minions, salt_cache['hostname'])
 
   return data
