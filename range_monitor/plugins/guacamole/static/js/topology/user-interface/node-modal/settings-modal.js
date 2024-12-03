f04dde37d194aee7275656b539954038e8abf4d6
rhawk117 <hawkinsry19@gmail.com>
2024-12-03 14:37:54 -0500

    chore: refactoring modal builder with better modularity.

--- range_monitor/plugins/guacamole/static/js/topology/user-interface/node-modal/settings-modal.js
+++ range_monitor/plugins/guacamole/static/js/topology/user-interface/node-modal/settings-modal.js
@@ -4,7 +4,6 @@ import {
   ModalTab,
 } from "./components/modal-assets.js";
 
-
 const modalIcons = {
   info: "fa-solid fa-info",
   settings: "fa-solid fa-gears",
@@ -17,94 +16,102 @@ export function settingsModalData(nodeContext, scheduler, settings) {
     throw new Error("No context was provided for the settings modal");
   }
   const overviewTab = new ModalTab("Topology Overview", modalIcons.info);
-  buildOverviewTab(nodeContext, scheduler, overviewTab);
+  settingsBuilder.buildOverviewTab(nodeContext, scheduler, overviewTab);
   const preferenceTab = new ModalTab("Preferences", modalIcons.settings);
-  initSettingControls(settings, scheduler, preferenceTab);
+  settingsBuilder.initSettingControls(settings, scheduler, preferenceTab);
   return [overviewTab, preferenceTab];
 }
 
-function buildOverviewTab(nodeContext, { lastUpdated, upTime }, overviewTab) {
-  const activeCount = nodeContext.countActiveConnections();
-  const groupNodes = nodeContext.filterBy((node) => node.isGroup());
-  
-  let activeGroups = 0; 
-  groupNodes.forEach(group => {
-    const childNodes = nodeContext.filterBy((node) => {
-      return node.parentIdentifier === group.identifier
+
+const settingsBuilder = {
+  buildOverviewTab(nodeContext, { lastUpdated, upTime }, overviewTab) {
+    const activeCount = nodeContext.countActiveConnections();
+    const groupNodes = nodeContext.filterBy((node) => node.isGroup());
+    
+    let activeGroups = 0; 
+    groupNodes.forEach(group => {
+      const childNodes = nodeContext.filterBy((node) => {
+        return node.parentIdentifier === group.identifier
+      });
+      const activeCount = childNodes.filter((node) => node.isActive()).length || 0;
+      activeGroups += (activeCount > 0) ? 1 : 0;
     });
-    const activeCount = childNodes.filter((node) => node.isActive()).length || 0;
-    activeGroups += (activeCount > 0) ? 1 : 0;
-  });
 
-  overviewTab.addContent([
-    Field.create("Total Connections", nodeContext.size),
-    Field.create("Active Connections", activeCount),
-    Field.create("Inactive Connections", nodeContext.size - activeCount),
-    Field.create("Total Connection Groups", groupNodes.length),
-    Field.create("Number of Active Connection Groups", activeCount),
-  ]);
+    overviewTab.addContent([
+      Field.create("Total Connections", nodeContext.size),
+      Field.create("Active Connections", activeCount),
+      Field.create("Inactive Connections", nodeContext.size - activeCount),
+      Field.create("Total Connection Groups", groupNodes.length),
+      Field.create("Number of Active Connection Groups", activeCount),
+    ]);
 
-  const $timeCollapsible = settingsTimeData(lastUpdated, upTime)
-  overviewTab.addContent($timeCollapsible);
-}
+    const $timeCollapsible = this.settingsTimeData(lastUpdated, upTime)
+    overviewTab.addContent($timeCollapsible);
+  },
 
-function settingsTimeData(lastUpdated, upTime) {
-  const uptimeCollapsible = new Collapsible("Time Information");
-  uptimeCollapsible.addHeaderIcon(modalIcons.time);
-  
-  const upTimeString = new Date(upTime).toLocaleString();
+  settingsTimeData(lastUpdated, upTime) {
+    const uptimeCollapsible = new Collapsible("Time Information");
+    uptimeCollapsible.addHeaderIcon(modalIcons.time);
 
-  const startField = new Field("Topology Start", upTimeString);
-  const uptimeField = new Field("Topology Uptime", upTimeString);
-  const lastUpdatedField = new Field("Last Update", new Date(lastUpdated).toLocaleString());
-  const refreshCountdown = new Field("Next Update", "00:00:00");
-  uptimeCollapsible.addContent([
-    startField.toHTML(),
-    lastUpdatedField.toHTML(),
-    uptimeField.toHTML({ valueId: "uptime-field" }),
-    refreshCountdown.toHTML({valueId: "refresh-countdown"}),
-  ]);
-  return uptimeCollapsible.$container;
-}
+    const upTimeString = new Date(upTime).toLocaleString();
 
-function initSettingControls(settings, { stringDelay, delay }, preferenceTab) {
-  console.log(`refresh speed: ${delay}`);
-  const determineStatus = (flag) => flag ? "active" : "inactive";
-  const determineBtnIcon = (flag) => flag ? "fas fa-check" : "fas fa-times";
-  const determineCheckboxIcon = (flag) => flag ? "fas fa-check-square" : "far fa-square";
-  const { showInactive, refreshEnabled } = settings;
-  // NOTE - maybe try using templates
-  const $controls = $("<div>", {class: "settings-controls"}).html(`
-    <div class="toggle-button ${determineStatus(showInactive)}" id="toggle-show-inactive">
-      <i class="${determineBtnIcon(showInactive)}"></i>
-      <span>Show Inactive Nodes</span>
-    </div>
+    const startField = new Field("Topology Start", upTimeString);
+    const uptimeField = new Field("Topology Uptime", upTimeString);
+    const lastUpdatedField = new Field("Last Update", new Date(lastUpdated).toLocaleString());
+    const refreshCountdown = new Field("Next Update", "00:00:00");
+    uptimeCollapsible.addContent([
+      startField.toHTML(),
+      lastUpdatedField.toHTML(),
+      uptimeField.toHTML({ valueId: "uptime-field" }),
+      refreshCountdown.toHTML({valueId: "refresh-countdown"}),
+    ]);
+    return uptimeCollapsible.$container;
+  },
 
-    <div class="toggle-button ${determineStatus(refreshEnabled)}" id="toggle-enable-refresh">
-      <i class="${determineBtnIcon(refreshEnabled)}"></i>
-      <span>Enable Refresh</span>
-    </div>
-
-    <div class="option-group refresh-speed">
-      <div class="sub-option speed-option" data-speed="low">
-        <i class="${determineCheckboxIcon(stringDelay === "low")}"></i>
-        <span>Low <i>(30s)</i></span>
-        <i class="fas fa-walking speed-icon"></i>
+  initSettingControls(settings, { stringDelay, delay }, preferenceTab) {
+    console.log(`refresh speed: ${delay}`);
+    const determineStatus = (flag) => flag ? "active" : "inactive";
+    const determineBtnIcon = (flag) => flag ? "fas fa-check" : "fas fa-times";
+    const determineCheckboxIcon = (flag) => flag ? "fas fa-check-square" : "far fa-square";
+    const { showInactive, refreshEnabled } = settings;
+    // NOTE - maybe try using templates
+    const $controls = $("<div>", {class: "settings-controls"}).html(`
+      <div class="toggle-button ${determineStatus(showInactive)}" id="toggle-show-inactive">
+        <i class="${determineBtnIcon(showInactive)}"></i>
+        <span>Show Inactive Nodes</span>
       </div>
-      <div class="sub-option speed-option" data-speed="medium">
-        <i class="${determineCheckboxIcon(stringDelay === "medium")}"></i>
-        <span>Medium <i>(15s) [default]</i></span>
-        <i class="fas fa-adjust speed-icon"></i>
+
+      <div class="toggle-button ${determineStatus(refreshEnabled)}" id="toggle-enable-refresh">
+        <i class="${determineBtnIcon(refreshEnabled)}"></i>
+        <span>Enable Refresh</span>
       </div>
-      <div class="sub-option speed-option" data-speed="high">
-        <i class="${determineCheckboxIcon(stringDelay === "high")}"></i>
-        <span>High <i>(5s)</i> </span> 
-        <i class="fas fa-tachometer-alt speed-icon"></i>
-      </div>
-    </div>  
-  `);
-  preferenceTab.addContent($controls);
-}
+
+      <div class="option-group refresh-speed">
+        <div class="sub-option speed-option" data-speed="low">
+          <i class="${determineCheckboxIcon(stringDelay === "low")}"></i>
+          <span>Low <i>(30s)</i></span>
+          <i class="fas fa-walking speed-icon"></i>
+        </div>
+        <div class="sub-option speed-option" data-speed="medium">
+          <i class="${determineCheckboxIcon(stringDelay === "medium")}"></i>
+          <span>Medium <i>(15s) [default]</i></span>
+          <i class="fas fa-adjust speed-icon"></i>
+        </div>
+        <div class="sub-option speed-option" data-speed="high">
+          <i class="${determineCheckboxIcon(stringDelay === "high")}"></i>
+          <span>High <i>(5s)</i> </span> 
+          <i class="fas fa-tachometer-alt speed-icon"></i>
+        </div>
+      </div>  
+    `);
+    preferenceTab.addContent($controls);
+  }
+};
+
+
+
+
+
 
 
 
