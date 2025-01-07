060b83daaa651b937eae62226f1e7052cfc84ccf
rhawk117 <hawkinsry19@gmail.com>
2025-01-07 16:16:48 -0500

    chore: added config for routes for easier management

--- version_2/api/main/datasources/routes.py
+++ version_2/api/main/datasources/routes.py
@@ -1,4 +1,60 @@
+from fastapi import APIRouter
+from api.models.data_source import Guacamole, Openstack, Saltstack
+from api.models.schemas import (
+    GuacRead, GuacCreate, GuacUpdate,
+    OpenstackRead, OpenstackCreate, OpenstackUpdate,
+    SaltstackRead, SaltstackCreate, SaltstackUpdate
+)
+from api.main.datasources.router_factory import create_data_source_router
 
+ROUTE_CONFIGS: list[dict] = [
+    {
+        'datasource_model': Guacamole,
+        'create_schema': GuacCreate,
+        'read_schema': GuacRead,
+        'update_schema': GuacUpdate,
+        'datasource_name': 'guac'
+    },
+    {
+        'datasource_model': Openstack,
+        'create_schema': OpenstackCreate,
+        'read_schema': OpenstackRead,
+        'update_schema': OpenstackUpdate,
+        'datasource_name': 'openstack'
+    },
+    {
+        'datasource_model': Saltstack,
+        'create_schema': SaltstackCreate,
+        'read_schema': SaltstackRead,
+        'update_schema': SaltstackUpdate,
+        'datasource_name': 'saltstack'
+    }    
+]
+
+def create_datasource_router() -> APIRouter:
+    '''
+    Creates the API Router for all of the Datasources
+    using ROUTE_CONFIGS and create_data_source_router
+    factory function 
+    
+    Returns:
+        APIRouter -- the datasource API router
+    '''    
+    ds_router = APIRouter(prefix='/datasources')
+    for route_config in ROUTE_CONFIGS:
+        print(f'[>] Creating route for {route_config["datasource_name"]}')
+        ds_router.include_router(
+            create_data_source_router(
+                **route_config
+            )   
+        )
+    return ds_router
+        
+    
+    
+    
+    
+    
 
 
 
