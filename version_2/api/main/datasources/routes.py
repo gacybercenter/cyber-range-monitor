b3ea0eb4a4c6be5562b5343237d55da302f61cb2
rhawk117 <hawkinsry19@gmail.com>
2025-01-07 15:04:37 -0500

    chore: laying out the directory structure for the datasource routes

---
+++ version_2/api/main/datasources/routes.py
@@ -0,0 +1,22 @@
+
+
+
+
+'''
+for each of the datasources
+
+/<datasource_name> [GET] - list all data sources
+/<datasource_name> [POST] - create a new data source
+
+/<datasource_name>/<datasource_id> [GET] - get a data source
+/<datasource_name>/<datasource_id> [PUT] - update a data source
+/<datasource_name>/<datasource_id> [DELETE] - delete a data source
+
+/<datasource_name>/<datasource_id>/ [POST] - toggle a datasource 
+
+
+but they each have different names and attributes 
+
+-create a generic service class?
+
+'''
