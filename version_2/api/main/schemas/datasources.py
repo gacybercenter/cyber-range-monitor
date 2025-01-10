7ce788208f3fb5cbd2ce8b6047efeede68a54fba
rhawk117 <hawkinsry19@gmail.com>
2025-01-10 15:53:26 -0500

    feat: stricter checks for pydantic data source models

---
+++ version_2/api/main/schemas/datasources.py
@@ -0,0 +1,127 @@
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    Field,
+    HttpUrl,
+    SecretStr,
+    SerializationInfo,
+    StringConstraints,
+    field_serializer,
+)
+from typing import Optional, Annotated
+
+LimitedStr = Annotated[str, StringConstraints(min_length=1, max_length=255)]
+
+Id_Api_Version = Annotated[str, StringConstraints(
+    min_length=1,
+    max_length=2,
+    pattern=r"^(2.0|3)$"
+)]
+
+Region = Annotated[str, StringConstraints(
+    min_length=1,
+    max_length=50,
+)]
+
+Hostname = Annotated[str, StringConstraints(
+    min_length=1,
+    max_length=253,
+)]
+
+
+class DatasourceReadBase(BaseModel):
+    id: int
+    username: str
+    password: str
+    enabled: bool
+
+
+class DatasourceCreateBase(BaseModel):
+    username: LimitedStr
+    password: LimitedStr
+    enabled: bool = Field(default=False)
+
+
+class DatasourceUpdateBase(BaseModel):
+    username: Optional[LimitedStr] = None
+    password: Optional[LimitedStr] = None
+    enabled: Optional[bool] = None
+
+
+class GuacRead(DatasourceReadBase):
+    datasource: LimitedStr
+    endpoint: HttpUrl
+
+
+class GuacCreate(DatasourceCreateBase):
+    datasource: LimitedStr
+    endpoint: HttpUrl
+
+
+class GuacUpdate(DatasourceUpdateBase):
+    datasource: Optional[LimitedStr] = None
+    endpoint: Optional[HttpUrl] = None
+
+
+class OpenstackRead(DatasourceReadBase):
+    auth_url: str
+    project_id: Optional[str] = None
+    project_name: Optional[str] = None
+    project_domain_name: str
+    user_domain_name: str
+    region_name: str
+    identity_api_version: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class OpenstackCreate(DatasourceCreateBase):
+    auth_url: HttpUrl
+
+    project_id: Optional[LimitedStr] = None
+    project_name: Optional[LimitedStr] = None
+    project_domain_name: Optional[LimitedStr] = None
+
+    user_domain_name: LimitedStr
+    region_name: Region
+    identity_api_version: Id_Api_Version
+
+
+class OpenstackUpdate(DatasourceUpdateBase):
+    auth_url: Optional[HttpUrl] = None
+    project_id: Optional[LimitedStr] = None
+    project_name: Optional[LimitedStr] = None
+    project_domain_name: Optional[LimitedStr] = None
+    user_domain_name: Optional[LimitedStr] = None
+    region_name: Optional[LimitedStr] = None
+    identity_api_version: Optional[Id_Api_Version] = None
+
+
+class SaltstackRead(DatasourceReadBase):
+    endpoint: HttpUrl
+    hostname: str
+
+
+class SaltstackCreate(DatasourceCreateBase):
+    endpoint: HttpUrl
+    hostname: Hostname
+
+
+class SaltstackUpdate(DatasourceUpdateBase):
+    endpoint: Optional[HttpUrl] = None
+    hostname: Optional[Hostname] = None
+
+
+def main() -> None:
+    guac = GuacCreate(
+        username='admin',
+        password='admin',
+        enabled=True,
+        datasource='guacamole',
+        endpoint='http://localhost:8080/guacamole'
+    )
+    print(guac.model_dump(mode='json'))
+
+
+if __name__ == '__main__':
+    main()
---
+++ version_2/api/main/schemas/validate.py
@@ -0,0 +1,26 @@
+from api.models.user import UserRoles
+
+
+class SchemaCheck:
+    '''
+        static methods to check the validity of the pydantic models 
+    '''
+
+    @staticmethod
+    def check_permission(user_permission: str) -> None:
+        assert user_permission in [
+            perm.value for perm in UserRoles
+        ], ERROR_MSG.UNKNOWN_ROLE
+
+    @staticmethod
+    def no_space(field: str, field_name: str) -> None:
+        assert not ' ' in field, f'{field_name} {ERROR_MSG.HAS_SPACE}'
+
+
+
+class ERROR_MSG:
+    INVALID_ROLE = 'You do not have permission to assign this role'
+    UNKNOWN_ROLE = 'Cannot assign an unkown role to a user'
+    HAS_SPACE = 'Field cannot contain space'
+
+    BAD_API_VERSION = 'API version must be a string of digits'
