4e29b52115f2c05d4c49a711fbf553891ce75714
rhawk117 <hawkinsry19@gmail.com>
2025-01-10 15:52:45 -0500

    chore: moved schema file to seperate directory

--- version_2/api/models/schemas/user_schema.py
+++ version_2/api/models/schemas/user_schema.py
@@ -1,18 +1,31 @@
 from pydantic import BaseModel, Field, model_validator, ConfigDict
-from api.models.user import UserRoles
 from typing import Optional
 from datetime import datetime
+
 from api.models.schemas.shared import SchemaCheck
-    
+from api.models.user import UserRoles
+
+
+class LoginRequest(BaseModel):
+    username: str = Field(..., min_length=3, max_length=100)
+    password: str = Field(..., min_length=3, max_length=100)
+
+    @model_validator(mode='after')
+    def check_creds(cls, v):
+        SchemaCheck.no_space(v.username, 'Username')
+        SchemaCheck.no_space(v.password, 'Password')
+        return v
+
+
 class ReadUser(BaseModel):
-    id: int 
+    id: int
     username: str
     permission: str
-    password_hash: str 
-    
+    password_hash: str
+
     created_at: datetime
     updated_at: datetime
-    
+
     model_config = ConfigDict(from_attributes=True)
 
 
@@ -23,37 +36,25 @@ class BaseUser(BaseModel):
         min_length=3,
         max_length=20
     )
-    password: str  = Field(..., max_length=255)
+    password: str = Field(..., max_length=255)
 
     @model_validator(mode='after')
     def check_base_user(cls, v):
-        if v.permission: 
+        if v.permission:
             SchemaCheck.check_permission(v.permission)
-        if v.username: 
+        if v.username:
             SchemaCheck.no_space(v.username, 'Username')
         if v.password:
             SchemaCheck.no_space(v.password, 'Password')
         return v
 
+
 class CreateUser(BaseUser):
     pass
+
 
 class UpdateUser(BaseUser):
     '''optional fields for updating a user'''
     username: Optional[str] = Field(None, min_length=3, max_length=100)
-    permission: Optional[str] = Field(None, min_length=3, max_length=20)    
-    password: Optional[str] = Field(None, max_length=255)   
-
-    
-
-    
-    
-
-
-
-
-
-
-
-
-
+    permission: Optional[str] = Field(None, min_length=3, max_length=20)
+    password: Optional[str] = Field(None, max_length=255)
