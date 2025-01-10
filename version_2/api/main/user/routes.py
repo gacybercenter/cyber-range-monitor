2134b25ccd52cd85241b6441c4abbe0fb76fb971
rhawk117 <hawkinsry19@gmail.com>
2025-01-10 17:22:05 -0500

    feat: basic JWT implemented

--- version_2/api/main/user/routes.py
+++ version_2/api/main/user/routes.py
@@ -1,23 +1,69 @@
+from calendar import c
+from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
-from api.models.schemas.user_schema import CreateUser, UpdateUser, ReadUser
+from fastapi.security import (
+    OAuth2PasswordBearer,
+    OAuth2PasswordRequestForm
+)
+from api.main.schemas.user import CreateUser, UpdateUser, UserResponse
 from api.main.user.services import UserService
 from api.utils.dependencies import needs_db
+from api.utils.security.oauth import (
+    UserOAuthData, 
+    create_access_token
+)
 
+
+user_service = UserService()
+
+# ==================
+#     User Auth 
+# ==================
+
+auth_router = APIRouter(
+    prefix='/auth',
+    tags=['Authentication & Authorization']
+)
+
+
+@auth_router.post('/auth/login')
+async def login_user(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: needs_db   
+):
+    user = await user_service.verify_credentials(
+        form_data.username, 
+        form_data.password, 
+        db
+    )
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid username or password'
+        )
+    access_token = create_access_token(
+        UserOAuthData(
+            username=user.username, # type: ignore
+            permission=user.permission # type: ignore
+        )
+    )
+    return {'access_token': access_token, 'token_type': 'bearer'}
+
+
+
+# ==================
+#     User CRUD
+# ==================
 
 user_router = APIRouter(
     prefix='/user',
     tags=['User']
 )
-user_service = UserService()
 
 
-
-@user_router.post('/register', response_model=ReadUser, status_code=status.HTTP_201_CREATED)
-async def create_user(
-    create_req: CreateUser,
-    db: needs_db
-) -> ReadUser:
+@user_router.post('/', response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+async def create_user(create_req: CreateUser, db: needs_db) -> UserResponse:
     '''
     creates a user given the request body schema
     Arguments:
@@ -27,7 +73,7 @@ async def create_user(
     Raises:
         HTTPException: 400 - Username is already taken
     Returns:
-        ReadUser -- the created user
+        UserResponse -- the created user
     '''
     username_taken = await user_service.username_is_taken(db, create_req.username)
     if username_taken:
@@ -40,12 +86,12 @@ async def create_user(
     return resulting_user  # type: ignore
 
 
-@user_router.put('/{user_id}', response_model=ReadUser, status_code=status.HTTP_200_OK)
+@user_router.put('/{user_id}', response_model=UserResponse, status_code=status.HTTP_200_OK)
 async def update_user(
     user_id: int,
     update_req: UpdateUser,
     db: needs_db
-) -> ReadUser:
+) -> UserResponse:
     '''
     updates the user given an id and uses the schema to update the user's data
 
@@ -55,25 +101,20 @@ async def update_user(
     Keyword Arguments:
         db {AsyncSession} -- (default: {Depends(get_db)})
     Returns:
-        ReadUser
+        UserResponse
     '''
-    
+
     updated_data = await user_service.update_user(db, user_id, update_req)
     return updated_data  # type: ignore
 
 
 @user_router.delete('/{user_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_user(user_id: int, db: needs_db) -> None:
-    if not await user_service.is_authenticated():
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail='Unauthorized'
-        )
     await user_service.delete_user(db, user_id)
 
 
-@user_router.get('/{user_id}', response_model=ReadUser, status_code=status.HTTP_200_OK)
-async def read_user(user_id: int, db: needs_db) -> ReadUser:
+@user_router.get('/{user_id}', response_model=UserResponse, status_code=status.HTTP_200_OK)
+async def read_user(user_id: int, db: needs_db) -> UserResponse:
     user = await user_service.get_by_id(user_id, db)
     if not user:
         raise HTTPException(
@@ -83,7 +124,7 @@ async def read_user(user_id: int, db: needs_db) -> ReadUser:
     return user  # type: ignore
 
 
-@user_router.get('/', response_model=list[ReadUser])
-async def read_all_users(db: needs_db) -> list[ReadUser]:
+@user_router.get('/', response_model=list[UserResponse])
+async def read_all_users(db: needs_db) -> list[UserResponse]:
     users = await user_service.get_all(db)
     return users  # type: ignore
---
+++ version_2/api/utils/security/oauth.py
@@ -0,0 +1,50 @@
+from typing import Annotated, Optional
+from pydantic import BaseModel
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+import jwt
+
+from api.config.settings import app_config
+from datetime import datetime, timedelta, timezone
+from fastapi import HTTPException, status
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='auth/login')
+# require_user = Annotated[str, Depends(get_current_user)]
+
+ACCESS_TOKEN_EXPIRE_MINUTES = timedelta(minutes=30)
+REFRESH_TOKEN_EXPIRE_MINUTES = timedelta(days=1)
+
+
+class UserOAuthData(BaseModel):
+    username: str
+    permission: str
+
+
+def create_access_token(
+    user_data: UserOAuthData,
+    expires_delta: timedelta = ACCESS_TOKEN_EXPIRE_MINUTES
+) -> str:
+    jwt_encoding = user_data.model_dump()
+    expires = datetime.now(timezone.utc) + expires_delta
+    jwt_encoding.update({'exp': expires})
+    return jwt.encode(
+        jwt_encoding,
+        app_config.JWT_SECRET_KEY,
+        algorithm=app_config.JWT_ALGORITHM
+    )
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    try:
+        payload = jwt.decode(
+            token,
+            app_config.JWT_SECRET_KEY,
+            algorithms=[app_config.JWT_ALGORITHM]
+        )
+        return UserOAuthData(**payload)
+    except jwt.PyJWTError:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='Invalid or expired token'
+        )
