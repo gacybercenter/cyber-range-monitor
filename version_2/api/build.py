5a1925864f94ccdf34fe35c75cb947dcc169f54d
rhawk117 <hawkinsry19@gmail.com>
2025-01-10 15:47:04 -0500

    feat: app supports custom error handling for routes

--- version_2/api/build.py
+++ version_2/api/build.py
@@ -2,8 +2,10 @@ from api.db.main import init_db
 from contextlib import asynccontextmanager
 from typing import Any, AsyncGenerator
 from fastapi import FastAPI
-
+from pydantic import ValidationError
+from fastapi.exceptions import HTTPException
 from api.main import user
+from api.utils.errors import handle_http_error, handle_validation_error
 
 
 @asynccontextmanager
@@ -14,6 +16,17 @@ async def life_span(app: FastAPI) -> AsyncGenerator:
     print("Shutting Down Application...")
 
 
+def register_exc_handlers(app: FastAPI) -> None:
+
+    @app.exception_handler(ValidationError)
+    async def validation_exception_handler(request, exc) -> Any:
+        return handle_validation_error(exc)
+
+    @app.exception_handler(HTTPException)
+    async def http_exception_handler(request, exc) -> Any:
+        return handle_http_error(request, exc)
+
+
 def register_routes(app: FastAPI) -> None:
     '''
     Registers all the routes from the APIRouters,
@@ -22,9 +35,9 @@ def register_routes(app: FastAPI) -> None:
     Arguments:
         app {FastAPI} -- app instance 
     '''
-    from api.main.user.user_routes import user_router
+    from api.main.user.routes import user_router
     from api.main.datasources.routes import create_datasource_router
-    
+
     # insert all routers here
     APP_ROUTES = [
         user_router,
@@ -33,7 +46,3 @@ def register_routes(app: FastAPI) -> None:
     for route in APP_ROUTES:
         print(f"[>] Registering {route.prefix} routes...")
         app.include_router(route)
-    
-    
-    
-    
