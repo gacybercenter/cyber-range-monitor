3a6603b1fbee562122e2c840335381cd3044958b
rhawk117 <hawkinsry19@gmail.com>
2025-01-10 15:55:20 -0500

    chore: removed old files

--- version_2/api/models/schemas/__init__.py
+++
@@ -1,3 +0,0 @@
-from .guac import GuacRead, GuacCreate, GuacUpdate
-from .openstack import OpenstackRead, OpenstackCreate, OpenstackUpdate
-from .saltstack import SaltstackRead, SaltstackCreate, SaltstackUpdate
