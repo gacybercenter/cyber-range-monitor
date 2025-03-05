4e313573d206434dfcba3ee18d3613534c1feb9c
rhawk117 <hawkinsry19@gmail.com>
2025-03-05 16:07:48 -0500

    chore: bug fixes

---
+++ backend/app/models/pk_id_mixin.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Integer
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+
+class PkIDModelMixin(object):
+    """generic model with an autoincremented ID thats a primary key
+    """
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True,
+        unique=True,
+        index=True
+    )
+    
