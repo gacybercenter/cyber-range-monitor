b7afd25f2b2d4f29c72726562f97606e5eae7821
rhawk117 <hawkinsry19@gmail.com>
2025-03-11 12:42:58 -0400

    fix: linting error

--- backend/app/datasources/schema/__init__.py
+++ backend/app/datasources/schema/__init__.py
@@ -1,3 +1,15 @@
 from .guacamole import GuacamoleCreateForm, GuacamoleRead, GuacamoleUpdateForm
 from .openstack import OpenstackCreateForm, OpenstackRead, OpenstackUpdateForm
 from .saltstack import SaltstackCreateForm, SaltstackRead, SaltstackUpdateForm
+
+__all__ = [
+    "GuacamoleCreateForm",
+    "GuacamoleRead",
+    "GuacamoleUpdateForm",
+    "OpenstackCreateForm",
+    "OpenstackRead",
+    "OpenstackUpdateForm",
+    "SaltstackCreateForm",
+    "SaltstackRead",
+    "SaltstackUpdateForm"
+]
